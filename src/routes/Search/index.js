6f81a54d1e783308e639c2d3c2ebb0c572cd416c
xiaosong <kunkkaco@gmail.com>
2018-04-12 14:07:07 +0800

    feat: nav

---
+++ src/routes/Search/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'dva';
+// import styles from './index.less';
+
+class Search extends React.PureComponent {
+  render() {
+    return (
+      <h1 style={{ textAlign: 'center', marginTop: '2em' }}>暂不支持，敬请期待...</h1>
+    );
+  }
+}
+
+Search.propTypes = {
+  repos: PropTypes.array,
+};
+
+export default connect(state => ({
+  repos: state.app.repos,
+}))(Search);
