43a4edb52b7816d575e0f06eb9401be0b91b106f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-23 01:41:00 +0000

    Deploy IssueReader/issue-reader-dva to github.com/IssueReader/issue-reader-dva.git:gh-pages

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import zhCN from 'antd/lib/locale-provider/zh_CN';
 import Loading from './components/Loading';
 import Updating from './routes/Updating';
 import Login from './routes/Login';
+import Mobile from './routes/Mobile';
 import BasicLayout from './layouts/BasicLayout';
 
 
@@ -20,6 +21,7 @@ function RouterConfig({ history, app }) {
         <Switch>
           <Route path="/" exact component={Updating} />
           <Route path="/login" exact component={Login} />
+          <Route path="/mobile" exact component={Mobile} />
           <Route path="(.*)" render={props => <BasicLayout {...props} app={app} />} />
         </Switch>
       </Router>
